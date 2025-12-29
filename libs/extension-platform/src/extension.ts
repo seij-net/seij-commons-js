062bfa07e636b87e1170fa7d8e4426744baac9a6
Sebastien Just <sebastien.just@seij.net>
2025-12-29 11:56:52 +0100

    Upgrade packages eslint, testing-library, react-test-renderer, typescript

--- libs/extension-platform/src/extension.ts
+++ libs/extension-platform/src/extension.ts
@@ -3,7 +3,5 @@ import { Extension } from "./interfaces";
 // Core extension
 export const coreExtension: Extension = {
   name: "seij.extension-platform",
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   activate: () => {},
 };
